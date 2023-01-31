d55171789e87e1fd9c8c65a31544eb779700d9d2
CarolynnFleming <cfcatpink@gmail.com>
2023-01-30 21:39:01 -0600

    creating supabase client and env file as well as routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,41 @@
-export default function App() {
-  return <h1>Hello World</h1>;
+import React from 'react';
+import {
+  BrowserRouter as Router, Switch, Route 
+} from 'react-router-dom';
+import Home from './Home';
+import ItalianiceDetail from './views/ItalianiceDetail';
+import ItalianiceList from './views/ItalianiceList';
+import PopcornDetail from './views/PopcornDetail';
+import PopcornList from './views/PopcornList';
+import About from './About';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route>
+            <About />
+          </Route>
+          <Route>
+            <PopcornList />
+          </Route>
+          <Route>
+              <PopcornDetail />
+          </Route>
+          <Route>
+            <ItalianiceList />
+          </Route>
+          <Route>
+            <ItalianiceDetail />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
+
+export default App;
