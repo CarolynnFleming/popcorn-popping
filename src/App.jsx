ab7f25d2aa8770fc964ee7a8ead17716d9c5eca4
CarolynnFleming <cfcatpink@gmail.com>
2023-02-03 19:37:34 -0600

    navbar set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import Navbar from './Navbar';
 
-//import {
-  //BrowserRouter as Router, Switch, Route 
-//} from 'react-router-dom';
-//import Home from './Home';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Home';
 //import ItalianiceDetail from './views/ItalianiceDetail';
-//import ItalianiceList from './views/ItalianiceList';
+import ItalianiceList from './views/ItalianiceList';
 //import PopcornDetail from './views/PopcornDetail';
-//import PopcornList from './views/PopcornList';
-//import About from './About';
+import PopcornList from './views/PopcornList';
+import About from './About';
+import Cart from './Cart';
+import Contact from './Contact';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
+   
+    <div className="container">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="About" element={<About />} />
+        <Route path="Popcorn" element={<PopcornList/>} />
+        <Route path="ItalianIce" element={<ItalianiceList />} />
+        <Route path="Cart" element={<Cart />} />
+        <Route path="Contact" element={<Contact />}/>
+      </Routes>
+     
+          
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,30 +1,174 @@
-import React from 'react'
-import cart from './assets/cart.png';
-import styles from './styles.css'
 
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import { Link } from 'react-router-dom';
 
-export default function Navbar() {
+const pages = ['About', 'Popcorn', 'ItalianIce', 'Cart', 'Contact'];
+const settings = ['Profile', 'Popcorn', 'ItalianIce', 'Tins', 'Mixes'];
+
+function ResponsiveAppBar() {
+
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+    navigate('./about');
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <nav className={styles.nav}>
-        <a href="/" className={styles.webname}> What's Popping Popcorn</a> 
-        <ul>
-            <li>
-                <a href="/about">About</a>
-            </li>
-             <li>
-                <a href="/popcornList">Popcorn</a>
-            </li>
-            <li>
-                <a href="/italianiceList">Italian Ice</a>
-            </li>
-            <li>
-                <a href="/cart"><img className='cart-img' src={cart} alt="cart image"/></a>
-            </li>
-            <li>
-                <a href="/contact">Contact</a>
+    <AppBar position="static" style={{ color: "yellow"}} >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters >
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.2rem',
+              color: 'red',
+              textDecoration: 'none',
+            }}
+          >
+            What's Popping Popcorn
+          </Typography>
 
-            </li>
-        </ul>
-    </nav>
-  )
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="red"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">
+                    <Link
+                    style={{ textDecoration: "none", color: "white"}} 
+                    to={`/${page}`}>
+                    {page}
+                    </Link>
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'red',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'red', display: 'block' }}
+              >
+                <Link style={{ textDecoration: "none", color: "red"}} to={`/${page}`}>
+                    {page}
+                    </Link>
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
+export default ResponsiveAppBar;
