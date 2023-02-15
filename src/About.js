743430ae27562ae55a4650f81a7bb329f0308a61
CarolynnFleming <cfcatpink@gmail.com>
2023-02-15 10:10:04 -0600

    about page done

--- src/About.js
+++ src/About.js
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import popcornimage from './assets/popcorn-images.jpeg'
+import Italainiceimage from './assets/Italian-ice.jpeg'
 export default function About() {
   return (
-    <div>About</div>
+    <div style={{ color:'green'}}>
+      <h1 style={{textAlign:'center', fontFamily:'Comfortaa'}}>We’re a business dedicated to <br/> having the best Popcorn and <br/>Italian Ice flavors to choose <br/>from
+</h1>
+<img src={Italainiceimage} />
+<img src={popcornimage} />
+<h2 style={{textAlign:'center', fontFamily:'Comfortaa'}}>Our Mission</h2>
+<p style={{textAlign:'center', fontFamily:'Comfortaa'}}>Build the best product that creates the most value <br/>for our customers, use business to inspire and <br/>implement environmentally friendly solutions.</p>
+<h2 style={{textAlign:'center', fontFamily:'Comfortaa'}}>Our Vission</h2>
+<p style={{textAlign:'center', fontFamily:'Comfortaa'}}>We strive to go above and beyond for our clients no <br/>matter the challenge. We aim to deliver our very <br/>best work every single day across our services.</p>
+    </div>
+   
   )
 }
--- src/Home.js
+++ src/Home.js
@@ -5,8 +5,9 @@ import{ Link } from 'react-router-dom'
 import popcorn from './assets/popcorn.gif';
 export default function Home() {
   return (
-    <div style={{ width:1000, height: 700,
-    borderRadius: 10, marginTop: 20, color:'green', marginLeft: 100}}>
+    <div style={{ backgroundColor:'rgb(242,80,18)',
+    background: 'linear-gradient(90deg, rgba(242,80,18,1) 0%, rgba(155,245,20,1) 65%, rgba(255,250,0,1) 100%)', width:1000, height: 550,
+    borderRadius: 400, marginTop: 20, color:'green', marginLeft: 100, paddingTop:30, marginTop: 60}}>
         <img style={{marginTop:30, borderRadius: 20, height: 320, width:320, marginLeft: 350}}className='pop-img'alt="popcorn" src={popcorn}/>
         <h1 style={{display:'flex', flexDirection: 'column', xmarginBottom: 100, textAlign:'center', fontFamily: 'Comfortaa' }}>It all starts with the best popcorn seeds</h1>
         <h3 style={{textAlign:'center', fontFamily:'Comfortaa'}}> We serve Popcorn and Italian Ice</h3>
@@ -34,7 +35,7 @@ export default function Home() {
 
 
   boxShadow: 'rgba(44,187,99,.35) 0 -25px 18px -14px inset,rgba(44,187,99,.25) 0 1px 2px,rgba(44,187,99,.25) 0 2px 4px,rgba(44,187,99,.25) 0 4px 8px,rgba(44,187,99,.25) 0 8px 16px,rgba(44,187,99,.25) 0 16px 32px',
-  transform: 'scale(1.05) rotate(-1deg)',
+  
   marginLeft: 400
 }}>Welcome Click to Shop</Link>
         </div>
