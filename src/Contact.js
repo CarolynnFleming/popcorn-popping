feea6245b184a440524b9312ce04a7c49bb272de
CarolynnFleming <cfcatpink@gmail.com>
2023-02-15 13:06:38 -0600

    re adjusted the styles on home

--- src/Contact.js
+++ src/Contact.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div style={{ fontFamily:'Comfortaa', color:'green', textAlign: 'center'}}>
+      <h1 style={{paddingTop: '90px', flexDirection: 'column'}}>You can reach us at</h1>
+      <h4 style={{paddingTop: '50px'}}>EMAIL:</h4>
+      <h2>Admin@whatspoppingpopcorn.com</h2>
+      <h4 style={{paddingTop: '50px'}}>PHONE NUMBER:</h4>
+      <h2>708-897-9757</h2>
+      <h4 style={{paddingTop: '50px'}}>LOCATION:</h4>
+      <h2> 11517 S. Pulaski Rd, Alsip, IL 60803</h2>
+      </div>
   )
 }
+
--- src/Home.js
+++ src/Home.js
@@ -3,17 +3,36 @@ import React from 'react'
 import{ Link } from 'react-router-dom'
 
 import popcorn from './assets/popcorn.gif';
+import { createTheme,ThemeProvider } from '@mui/material/styles';
+import Button from '@mui/material/Button'
+import { red } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#f2bb05',
+      darker: '#00a152',
+    },
+    
+  },
+});
+
 export default function Home() {
   return (
-    <div style={{ backgroundColor:'rgb(242,80,18)',
-    background: 'linear-gradient(90deg, rgba(242,80,18,1) 0%, rgba(155,245,20,1) 65%, rgba(255,250,0,1) 100%)', width:1000, height: 550,
-    borderRadius: 400, marginTop: 20, color:'green', marginLeft: 100, paddingTop:30, marginTop: 60}}>
-        <img style={{marginTop:30, borderRadius: 20, height: 320, width:320, marginLeft: 350}}className='pop-img'alt="popcorn" src={popcorn}/>
+    <div style={{ backgroundColor: 'red', width:900, height: 550,
+    borderRadius: 400, marginTop: 20, color:'White', marginLeft: 200, paddingTop:30, marginTop: 60, boxShadow: '150px 70px  80px #941c2f', marginBottom: 30}}>
+      <Link to="/Popcorn" style={{textDecoration:'none', color:'white'}}>
+        <img style={{marginTop:30, borderRadius: 20, height: 320, width:300, marginLeft: 320, boxShadow: '60px 40px 80px #f2bb05'}}className='pop-img'alt="popcorn" src={popcorn}/>
         <h1 style={{display:'flex', flexDirection: 'column', xmarginBottom: 100, textAlign:'center', fontFamily: 'Comfortaa' }}>It all starts with the best popcorn seeds</h1>
         <h3 style={{textAlign:'center', fontFamily:'Comfortaa'}}> We serve Popcorn and Italian Ice</h3>
-        <Link to="/Popcorn" style={{
-
-/* CSS */
+        <ThemeProvider theme={theme}>
+          <Button sx={{ alignItems: 'center', marginLeft: 45}}color="primary" variant="contained">
+            <Link to="/Popcorn" style={{color: 'white', textDecoration: 'none', textAlign: 'center'}}>
+              Welcome Click to Shop
+            </Link>
+          </Button>
+        </ThemeProvider>
+        {/* <Link to="/Popcorn" style={{
 
   backgroundColor: '#c2fbd7',
   borderRadius: 100,
@@ -37,7 +56,10 @@ export default function Home() {
   boxShadow: 'rgba(44,187,99,.35) 0 -25px 18px -14px inset,rgba(44,187,99,.25) 0 1px 2px,rgba(44,187,99,.25) 0 2px 4px,rgba(44,187,99,.25) 0 4px 8px,rgba(44,187,99,.25) 0 8px 16px,rgba(44,187,99,.25) 0 16px 32px',
   
   marginLeft: 400
-}}>Welcome Click to Shop</Link>
+
+
+}}>Welcome Click to Shop</Link> */}
+</Link>
         </div>
   )
 }
